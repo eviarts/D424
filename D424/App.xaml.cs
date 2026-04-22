1f8c716cc55a41a65c3a1f8bcf0fe5363964dfc3
ecrose1 <ecrose1@wgu.edu>
2026-04-21 21:24:03 -0400

    Added login page

--- D424/App.xaml.cs
+++ D424/App.xaml.cs
@@ -13,11 +13,27 @@ namespace C971App
             InitializeComponent();
 
             db = new Database();
+
+            CheckLoginStatus();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+
+        private async void CheckLoginStatus()
+        {
+            var token = await SecureStorage.GetAsync("auth_token");
+
+            if (string.IsNullOrEmpty(token))
+            {
+                MainPage = new LoginPage();
+            }
+            else
+            {
+                MainPage = new AppShell();
+            }
         }
     }
 }
---
+++ D424/LoginPage.xaml.cs
@@ -0,0 +1,31 @@
+namespace C971App;
+
+public partial class LoginPage : ContentPage
+{
+	public LoginPage()
+	{
+		InitializeComponent();
+	}
+
+	private async void LoginButton(object sender, EventArgs e)
+	{
+		var username = UsernameEntry.Text;
+		var password = PasswordEntry.Text;
+
+		if (username == null || password == null)
+		{
+			await DisplayAlertAsync("Error", "Please enter a username and password", "Ok");
+			return;
+		}
+
+		if (username == "test" && password == "test")
+		{
+			await SecureStorage.SetAsync("auth_token", "logged_in");
+			App.Current.MainPage = new AppShell();
+		}
+		else
+		{
+			ErrorLabel.Text = "Incorrect username or password";
+		}
+	}
+}
