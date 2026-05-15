6920792262a1d7a0024b0ff567ae8e82092bdf65
ecrose1 <ecrose1@wgu.edu>
2026-05-14 20:58:44 -0400

    Finished application v1.0.2

--- D424/LoginPage.xaml.cs
+++ D424/LoginPage.xaml.cs
@@ -9,28 +9,33 @@ public partial class LoginPage : ContentPage
 
 	private async void LoginButton(object sender, EventArgs e)
 	{
-		var username = UsernameEntry.Text;
-		var password = PasswordEntry.Text;
-
-		if (username == null || password == null)
-		{
-			await DisplayAlert("Error", "Please enter a username and password", "Ok");
-			return;
-		}
-
-		if (username == "test" && password == "test")
-		{
-			await SecureStorage.SetAsync("auth_token", "logged_in");
-			App.Current.MainPage = new AppShell();
-		}
-		else
-		{
-			ErrorLabel.Text = "Incorrect username or password";
-
-			UsernameEntry.Text = string.Empty;
-			PasswordEntry.Text = string.Empty;
-
-			UsernameEntry.Focus();
-		}
+        await LoginLogic();
 	}
+
+	private async Task LoginLogic()
+	{
+        var username = UsernameEntry.Text;
+        var password = PasswordEntry.Text;
+
+        if (username == null || password == null)
+        {
+            await DisplayAlert("Error", "Please enter a username and password", "Ok");
+            return;
+        }
+
+        if (username == "test" && password == "test")
+        {
+            await SecureStorage.SetAsync("auth_token", "logged_in");
+            App.Current.MainPage = new AppShell();
+        }
+        else
+        {
+            ErrorLabel.Text = "Incorrect username or password";
+
+            UsernameEntry.Text = string.Empty;
+            PasswordEntry.Text = string.Empty;
+
+            UsernameEntry.Focus();
+        }
+    }
 }
